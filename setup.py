a9e97a5574231e765a36ba476ce674b31dd61b1e
Nikhilnaidu10 <45360061+Nikhilnaidu10@users.noreply.github.com>
2020-06-23 15:16:45 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='nlp_test',
+    version='0.0.27',
+    description='testing tensorflow package intallation',
+    author='Nikhilnaidu10',
+    url='https://github.com/Nikhilnaidu10/test_setup',
+    packages=['nlp'],
+    install_requires=[
+        'tensorflow'
+    ]
+)
